240d02b8252587250afc714ebee86be87963489d
fernando.carvalho <fernando.carvalho@summit-bra.com>
2021-09-13 16:47:15 -0300

    start header tailwind

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import { Div } from "../../style/General.styled";
-import { BgHeader, LinkHeader, DivLink } from "./Header.styled";
 
 const Header = () => {
   return (
-    <>
-      <BgHeader>
-        <Div display="flex" alignItens="center">
-          <DivLink>
-            <LinkHeader to="/">SEARCH YOUR TEAM!</LinkHeader>
-          </DivLink>
-        </Div>
-      </BgHeader>
-    </>
+    <div className="flex flex-row items-center w-screen h-16 bg-green-200 border-b-8 border-green-300 text-center justify-centertext-center justify-center">
+      <h1 className="font-mono text-lg font-bold text-green-800 cursor-default mx-2">
+        Search Soccer Team
+      </h1>
+      <input
+        type="text"
+        className="mx-2 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      />
+      <button className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-3 rounded focus:outline-none focus:shadow-outline">
+        Search
+      </button>
+    </div>
   );
 };
 
